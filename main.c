a2c3893efaad777a6c54ac0a04734d0e89e95a03
leesoosu <144989207+leesoosu@users.noreply.github.com>
2023-11-02 11:13:13 +0900

    2_1

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N_STUDENT 5
+#define SIZE 5
 
 int main(int argc, char *argv[])
 {
   int i;
-  int grade[N_STUDENT];
+  int grade[SIZE];
+  printf("5명의 점수를 입력하세요. \n");
   
-  for(i=0; i<N_STUDENT; i++)
-    grade[i] = (i+1) *10;
-  
-  for(i=0; i<N_STUDENT; i++)
-    printf("grade[%i] = %i\n", i,grade[i]);
+  for(i=0; i<SIZE; i++)
+   scanf("%d", &grade[i]);
+   
+  for( i=0; i<SIZE; i++)
+   printf("grade[%d] = %d\n", i, grade[i]);
+
     
   system("PAUSE");	
   return 0;
