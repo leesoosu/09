86bc5b8dfea3b0ddb424cfc6c4234cb659d52d9b
leesoosu <144989207+leesoosu@users.noreply.github.com>
2023-11-02 11:22:52 +0900

    4

--- main.c
+++ main.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5
+#define SIZE 4
 
-int main(int argc, char *argv[])
+void square_array(int a[], int size)
 {
-  int i,average;
-  int sum;
-  int grade[SIZE];
-  printf("학생 성적을 입력하세요 : ");
-  
-  sum = 0;
-  for(i=0; i<SIZE; i++){
-   scanf("%d", &grade[i]);
-   sum += grade[i];
+     int i;
+     for (i=0; i<size; i++)
+     a[i] = a[i] * a[i];
 }
-   
-  average = sum / SIZE;
-  printf("성적 평균 : %d\n", average);
 
-    
-  system("PAUSE");	
-  return 0;
+void print_array(int a[], int size)
+{
+     int i;
+     for (i=0; i<size; i++)
+         printf("%3d", a[i]);
+     printf("\n");
 }
+
+
+int main(void)
+{
+    int list[SIZE] = {1, 2, 3, 4};
+    
+    print_array(list, SIZE);
+    square_array(list, SIZE);
+    print_array(list, SIZE);
+    
+    system("PAUSE");	
+    return 0;
+}
+
