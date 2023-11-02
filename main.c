753d41ff0fd1b66b04a9793a4b72315388b156bc
leesoosu <144989207+leesoosu@users.noreply.github.com>
2023-11-02 11:16:44 +0900

    2_2

--- main.c
+++ main.c
@@ -5,15 +5,19 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
+  int i,average;
+  int sum;
   int grade[SIZE];
-  printf("5명의 점수를 입력하세요. \n");
+  printf("학생 성적을 입력하세요 : ");
   
-  for(i=0; i<SIZE; i++)
+  sum = 0;
+  for(i=0; i<SIZE; i++){
    scanf("%d", &grade[i]);
+   sum += grade[i];
+}
    
-  for( i=0; i<SIZE; i++)
-   printf("grade[%d] = %d\n", i, grade[i]);
+  average = sum / SIZE;
+  printf("성적 평균 : %d\n", average);
 
     
   system("PAUSE");	
